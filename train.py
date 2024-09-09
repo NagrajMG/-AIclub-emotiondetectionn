1d29e400376ec2af275a892fe5fc32a6781833d6
Nagraj Gaonkar <nagrajgaonkarmumbai749@gmail.com>
2024-09-09 14:53:40 +0530

    Update train.py

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ def train():
     X_train = X_train.reshape((X_train.shape[0], 48, 48, 1))
     X_val = X_val.reshape((X_val.shape[0], 48, 48, 1))
     X_test = X_test.reshape((X_test.shape[0], 48, 48, 1))
-    batch_size = 25
+    batch_size = 10
     # 加载数据集
     lr = 1e-5
     epochs = 200
