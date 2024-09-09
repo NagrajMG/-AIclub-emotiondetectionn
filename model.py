16cb16cea5bc85cf3b9d6540c97627cfe72aeda4
Nagraj Gaonkar <nagrajgaonkarmumbai749@gmail.com>
2024-09-09 15:10:18 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,50 +3,114 @@
 #-----------------------------------
 from tensorflow.keras.layers import Conv2D,Flatten,MaxPooling2D,Input,BatchNormalization,Dropout,Dense
 from tensorflow.keras.models import Model
-def create_model(input_shape = (48,48,1),num_classes=7):
+def build_net(img_width, img_height, img_depth, num_classes):
+   
+    net = Sequential(name='DCNN')
 
-    input = Input(shape=input_shape)
-    x = Conv2D(filters=256,kernel_size=3,activation='relu',padding='same')(input)
+    net.add(
+        Conv2D(
+            filters=64,
+            kernel_size=(5,5),
+            input_shape=(img_width, img_height, img_depth),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_1'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_1'))
+    net.add(
+        Conv2D(
+            filters=64,
+            kernel_size=(5,5),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_2'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_2'))
+    
+    net.add(MaxPooling2D(pool_size=(2,2), name='maxpool2d_1'))
+    net.add(Dropout(0.4, name='dropout_1'))
 
-    x = Conv2D(filters=512,kernel_size=3,activation='relu',padding='same')(x)
-    x = BatchNormalization()(x)
+    net.add(
+        Conv2D(
+            filters=128,
+            kernel_size=(3,3),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_3'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_3'))
+    net.add(
+        Conv2D(
+            filters=128,
+            kernel_size=(3,3),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_4'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_4'))
+    
+    net.add(MaxPooling2D(pool_size=(2,2), name='maxpool2d_2'))
+    net.add(Dropout(0.4, name='dropout_2'))
 
-    #
-    x = MaxPooling2D(pool_size=(2,2))(x)
-    x = Dropout(0.4)(x)
+    net.add(
+        Conv2D(
+            filters=256,
+            kernel_size=(3,3),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_5'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_5'))
+    net.add(
+        Conv2D(
+            filters=256,
+            kernel_size=(3,3),
+            activation='elu',
+            padding='same',
+            kernel_initializer='he_normal',
+            name='conv2d_6'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_6'))
+    
+    net.add(MaxPooling2D(pool_size=(2,2), name='maxpool2d_3'))
+    net.add(Dropout(0.5, name='dropout_3'))
 
-    x = Conv2D(filters=384,kernel_size=3,activation='relu',padding='same')(x)
-    x = BatchNormalization()(x)
-
-    x = MaxPooling2D(pool_size=(2,2))(x)
-    x = Dropout(0.4)(x)
-
-    x = Conv2D(filters=192,kernel_size=3,activation='relu',padding='same')(x)
-    x = BatchNormalization()(x)
-
-    x = MaxPooling2D(pool_size=(2,2))(x)
-    x = Dropout(0.4)(x)
-
-
-    x = Conv2D(filters=384,kernel_size=3,activation='relu',padding='same')(x)
-    x = BatchNormalization()(x)
-
-    x = MaxPooling2D(pool_size=(2,2))(x)
-    x = Dropout(0.4)(x)
-
-    x = Flatten()(x)
-
-    x = Dense(256,activation='relu')(x)
-    x = BatchNormalization()(x)
-
-    x = Dropout(0.3)(x)
-    x = Dense(num_classes,activation='softmax')(x)
-
-    return Model(input,x,name='fer_model')
-
+    net.add(Flatten(name='flatten'))
+        
+    net.add(
+        Dense(
+            128,
+            activation='elu',
+            kernel_initializer='he_normal',
+            name='dense_1'
+        )
+    )
+    net.add(BatchNormalization(name='batchnorm_7'))
+    
+    net.add(Dropout(0.6, name='dropout_4'))
+    
+    net.add(
+        Dense(
+            num_classes,
+            activation='softmax',
+            name='out_layer'
+        )
+    )
+    return net
 
 if __name__=='__main__':
-    model = create_model()
+    model = build_net(48,48,1,7)
     model.summary()
 
 
